aad91b9fe43ba5122d7ed38630a887fb362eb2d9
POOJANGHETIYA <ghetiyapoojan@gmail.com>
2024-08-05 10:09:34 +0530

    .

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello, World!');
+  res.json({
+    message: 'Hello World'
+  });
 });
 
 app.listen(port, () => {
